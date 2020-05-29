53364a1661704c80e124f067a22fb1cef70361ee
楊崇遠 <tony@yangchongyuande-MacBook-Pro.local>
2020-05-30 04:21:05 +0800

    create second page for user info

--- app/src/main/java/com/test/githubapi_mvvm/api/services/GithubService.kt
+++ app/src/main/java/com/test/githubapi_mvvm/api/services/GithubService.kt
@@ -3,10 +3,14 @@ package com.test.githubapi_mvvm.api.services
 import com.test.githubapi_mvvm.mode.GithubUserMode
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
 
     @GET("users")
     fun getAllUserList(@Query("since") since:Int,@Query("per_page") per_page:Int):Call<List<GithubUserMode>>
+
+    @GET("user/{login}")
+    fun getUserInfo(@Path("login") login:String):Call<>
 }
