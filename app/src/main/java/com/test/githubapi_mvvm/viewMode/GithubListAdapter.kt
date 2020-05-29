d5e9ebe448d11472bf345ba10cf5f58ff02d97e6
楊崇遠 <tony@yangchongyuande-MacBook-Pro.local>
2020-05-29 18:11:33 +0800

    create layout/vh_useritem

--- app/src/main/java/com/test/githubapi_mvvm/viewMode/GithubListAdapter.kt
+++ app/src/main/java/com/test/githubapi_mvvm/viewMode/GithubListAdapter.kt
@@ -1,6 +1,7 @@
 package com.test.githubapi_mvvm.viewMode
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.test.githubapi_mvvm.mode.GithubUserMode
@@ -8,16 +9,28 @@ import com.test.githubapi_mvvm.mode.GithubUserMode
 class GithubListAdapter(context: Context , data:List<GithubUserMode>):RecyclerView.Adapter<GithubUserListItem>() {
     val context = context
     val data = data
+    var itemEvent:itemEvent? = null
 
+    fun setItemClick(clickEvent:itemEvent){
+        itemEvent = clickEvent
+    }
     override fun getItemCount(): Int {
-
+        return  data.size
     }
 
     override fun onBindViewHolder(holder: GithubUserListItem, position: Int) {
+        val itemData = data[position]
 
+        holder.itemView.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                if(itemEvent == null)
+                    return
+                itemEvent!!.onItemClick(itemData)
+            }
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GithubUserListItem {
-
+        return GithubUserListItem()
     }
 }
--- app/src/main/java/com/test/githubapi_mvvm/viewMode/GithubUserListItem.kt
+++ app/src/main/java/com/test/githubapi_mvvm/viewMode/GithubUserListItem.kt
@@ -2,7 +2,11 @@ package com.test.githubapi_mvvm.viewMode
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.test.githubapi_mvvm.mode.GithubUserMode
 
+interface itemEvent{
+    fun onItemClick(data:GithubUserMode)
+}
 class GithubUserListItem(v:View):RecyclerView.ViewHolder(v) {
 
 }
